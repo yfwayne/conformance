dc1358533790bdaf7c0fa627b427c37e166cb618
Crane Chu <cranechu@gmail.com>
2020-04-01 18:21:39 +0800

    update format scripts

--- 01_admin_cmd/format_test.py
+++ 01_admin_cmd/format_test.py
@@ -8,6 +8,10 @@ def test_format_all_basic(nvme0):
     if not nvme0.supports(0x80):
         pytest.skip("format is not support")
 
+    orig_timeout = nvme0.timeout
+    nvme0.timeout = 100000
+    
+    nvme0.format(0).waitdone()
     nvme0.format(0, 0).waitdone()
     nvme0.format(0, 1).waitdone()
     with pytest.warns(UserWarning, match="ERROR status: 01/0a"):
@@ -19,12 +23,17 @@ def test_format_all_basic(nvme0):
         nvme0.format(0, 0, 0xfffffffb).waitdone()
     with pytest.warns(UserWarning, match="ERROR status: 00/0b"):
         nvme0.format(0, 1, 0xfffffffb).waitdone()
+
+    nvme0.timeout = orig_timeout
         
 
 def test_format_verify_data(nvme0, nvme0n1, verify):
     if not nvme0.supports(0x80):
         pytest.skip("format is not support")
 
+    orig_timeout = nvme0.timeout
+    nvme0.timeout = 100000
+        
     # prepare data buffer and IO queue
     read_buf = Buffer(512)
     write_buf = Buffer(512)
@@ -69,11 +78,16 @@ def test_format_verify_data(nvme0, nvme0n1, verify):
         nvme0n1.read(qpair, read_buf, 0, 1).waitdone()
         assert read_buf[10:21] != b'hello world'
 
+    nvme0.timeout = orig_timeout
+        
 
 def test_format_invalid_ses(nvme0, nvme0n1, verify):
     if not nvme0.supports(0x80):
         pytest.skip("format is not support")
 
+    orig_timeout = nvme0.timeout
+    nvme0.timeout = 100000
+        
     # prepare data buffer and IO queue
     read_buf = Buffer(512)
     write_buf = Buffer(512)
@@ -86,13 +100,18 @@ def test_format_invalid_ses(nvme0, nvme0n1, verify):
     with pytest.warns(UserWarning, match="ERROR status: 00/02"):
         nvme0.format(0, 7, 0xffffffff).waitdone()
     nvme0n1.read(qpair, read_buf, 0, 1).waitdone()
-    assert read_buf[10:21] == b'hello world'        
+    assert read_buf[10:21] == b'hello world'
+    
+    nvme0.timeout = orig_timeout
 
     
 def test_format_invalid_lbaf(nvme0, nvme0n1, verify):
     if not nvme0.supports(0x80):
         pytest.skip("format is not support")
 
+    orig_timeout = nvme0.timeout
+    nvme0.timeout = 100000
+        
     # prepare data buffer and IO queue
     read_buf = Buffer(512)
     write_buf = Buffer(512)
@@ -112,4 +131,6 @@ def test_format_invalid_lbaf(nvme0, nvme0n1, verify):
         nvme0.format(2, 7).waitdone()
     nvme0n1.read(qpair, read_buf, 0, 1).waitdone()
     assert read_buf[10:21] == b'hello world'        
+
+    nvme0.timeout = orig_timeout
     
