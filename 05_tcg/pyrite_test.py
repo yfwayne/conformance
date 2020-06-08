2f61550813e61f10d7249a0387072a9d09953029
Crane Chu <cranechu@gmail.com>
2020-06-09 07:41:50 +0800

    update script

--- 05_tcg/pyrite_test.py
+++ 05_tcg/pyrite_test.py
@@ -14,22 +14,22 @@ def test_pyrite_discovery0(nvme0):
 
     
 def test_tcg_create(nvme0, tcg):
-    tcg.take_ownership(b"liteon")
+    tcg.take_ownership(b"123456")
 
 
 def test_tcg_password_hack_resist(nvme0, tcg):
     """ cannot crack with the password list """
     
     # verify set new password first
-    tcg.set_new_passwd(b'liteon', b'liteon')
+    tcg.set_new_passwd(b'123456', b'123456')
 
     for line in urllib.request.urlopen(url):
         with pytest.raises(TcgError):
-            tcg.set_new_passwd(line.strip(), b'liteon')
+            tcg.set_new_passwd(line.strip(), b'123456')
 
     # authority locked out
     with pytest.raises(TcgError):
-        tcg.set_new_passwd(b'liteon', b'liteon')
+        tcg.set_new_passwd(b'123456', b'123456')
 
         
 def test_tcg_lock_hack_resist(nvme0, tcg, subsystem):
@@ -40,7 +40,7 @@ def test_tcg_lock_hack_resist(nvme0, tcg, subsystem):
     nvme0.reset()
     
     # verify set new password first
-    tcg.lock(b'liteon')
+    tcg.lock(b'123456')
 
     for line in urllib.request.urlopen(url):
         with pytest.raises(TcgError):
@@ -48,7 +48,7 @@ def test_tcg_lock_hack_resist(nvme0, tcg, subsystem):
 
     # authority locked out
     with pytest.raises(TcgError):
-        tcg.lock(b'liteon')
+        tcg.lock(b'123456')
 
         
 def test_tcg_revert_hack_resist(nvme0, tcg, subsystem):
@@ -70,71 +70,71 @@ def test_tcg_clear(nvme0, tcg, subsystem):
     
     # revert need longer time
     nvme0.timeout = 100000
-    tcg.revert_tper(b"liteon")
+    tcg.revert_tper(b"123456")
     nvme0.timeout = 10000
 
     # 2nd try, nothing to revert
     with pytest.raises(TcgError):
-        tcg.revert_tper(b"liteon")
+        tcg.revert_tper(b"123456")
 
 
 def test_tcg_revert_with_passwd(nvme0, tcg):
-    tcg.take_ownership(b"liteon")
+    tcg.take_ownership(b"123456")
     
     with pytest.raises(TcgError):
         tcg.revert_tper(b"wrong_passwd")
 
     # revert need longer time
     nvme0.timeout = 100000
-    tcg.revert_tper(b"liteon")
+    tcg.revert_tper(b"123456")
     nvme0.timeout = 10000
 
 
 def test_tcg_lock_unlock(nvme0, nvme0n1, tcg, buf):
-    tcg.take_ownership(b"liteon")
+    tcg.take_ownership(b"123456")
     
     q = Qpair(nvme0, 10)
     nvme0n1.read(q, buf, 0).waitdone()
     nvme0n1.write(q, buf, 0).waitdone()
 
     # lock both read and write
-    tcg.lock(b'liteon')
+    tcg.lock(b'123456')
     with pytest.warns(UserWarning, match="ERROR status: 02/86"):
         nvme0n1.read(q, buf, 0).waitdone()
     with pytest.warns(UserWarning, match="ERROR status: 02/86"):
         nvme0n1.write(q, buf, 0).waitdone()
 
     # lock write
-    tcg.lock(b'liteon', 1)
+    tcg.lock(b'123456', 1)
     nvme0n1.read(q, buf, 0).waitdone()
     with pytest.warns(UserWarning, match="ERROR status: 02/86"):
         nvme0n1.write(q, buf, 0).waitdone()
 
     # unlock
-    tcg.lock(b'liteon', 4)
+    tcg.lock(b'123456', 4)
     nvme0n1.read(q, buf, 0).waitdone()
     nvme0n1.write(q, buf, 0).waitdone()
 
     # revert need longer time
     nvme0.timeout = 100000
-    tcg.revert_tper(b"liteon")
+    tcg.revert_tper(b"123456")
     nvme0.timeout = 10000
     
 
 def test_tcg_pasword(nvme0, tcg):
-    tcg.take_ownership(b"liteon")
+    tcg.take_ownership(b"123456")
 
-    tcg.set_new_passwd(b'liteon', b'abcd1234')
-    with pytest.raises(d.TcgError): # wrong password
-        tcg.set_new_passwd(b'liteon', b'abcd1234')
+    tcg.set_new_passwd(b'123456', b'abcd1234')
+    with pytest.raises(TcgError): # wrong password
+        tcg.set_new_passwd(b'123456', b'abcd1234')
     tcg.set_new_passwd(b'abcd1234', b'1')
     tcg.set_new_passwd(b'1', b'1'*32)
-    with pytest.raises(d.TcgError): # too long password
+    with pytest.raises(TcgError): # too long password
         tcg.set_new_passwd(b'1'*32, b'1'*33)
-    tcg.set_new_passwd(b'1'*32, b'ssstc')
+    tcg.set_new_passwd(b'1'*32, b'654321')
     
     # revert need longer time
     nvme0.timeout = 100000
-    tcg.revert_tper(b"ssstc")
+    tcg.revert_tper(b"654321")
     nvme0.timeout = 10000
     
