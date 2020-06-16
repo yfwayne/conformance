a6d2760e7e7afa4ddd21551887fcc49f357c4a8d
Crane Chu <cranechu@gmail.com>
2020-06-16 08:19:02 +0800

    add more files

--- 03_features/power_management_test.py
+++ 03_features/power_management_test.py
@@ -17,3 +17,13 @@ def test_host_controlled_thermal_management_enabled(nvme0):
         pytest.skip("APST is not enabled")
 
     pass
+
+
+@pytest.mark.parametrize("ps", [4, 3, 2, 1, 0])
+def test_format_at_power_state(nvme0, nvme0n1, ps):
+    nvme0.setfeatures(0x2, cdw11=ps).waitdone()
+    assert nvme0n1.format(ses=0) == 0
+    assert nvme0n1.format(ses=1) == 0
+    p = nvme0.getfeatures(0x2).waitdone()
+    assert p == ps
+
