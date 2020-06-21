1b473a194cf78df502090d000b66ff19f7d45f1b
Crane Chu <cranechu@gmail.com>
2020-06-21 20:47:17 +0800

    scripts: ioworker with wrr

--- 05_controller/arbitration_test.py
+++ 05_controller/arbitration_test.py
@@ -6,53 +6,91 @@ from nvme import Controller, Namespace, Buffer, Qpair, Pcie, Subsystem
 from scripts.psd import IOCQ, IOSQ, PRP, PRPList, SQE, CQE
 
 
-def test_weighed_round_robin(pcie):
-    def nvme_init(nvme0):
-        logging.info("user defined nvme init")
+def nvme_init_wrr(nvme0):
+    logging.info("user defined nvme init")
         
-        nvme0[0x14] = 0
-        while not (nvme0[0x1c]&0x1) == 0: pass
+    nvme0[0x14] = 0
+    while not (nvme0[0x1c]&0x1) == 0: pass
 
-        # 3. set admin queue registers
-        nvme0.init_adminq()
+    # 3. set admin queue registers
+    nvme0.init_adminq()
 
-        # 4. set register cc
-        if (nvme0.cap>>17) & 0x1:
-            logging.info("set WRR arbitration")
-            nvme0[0x14] = 0x00460800
-        else:
-            nvme0[0x14] = 0x00460000
+    # 4. set register cc
+    if (nvme0.cap>>17) & 0x1:
+        logging.info("set WRR arbitration")
+        nvme0[0x14] = 0x00460800
+    else:
+        nvme0[0x14] = 0x00460000
 
-        # 5. enable cc.en
-        nvme0[0x14] = nvme0[0x14] | 1
+    # 5. enable cc.en
+    nvme0[0x14] = nvme0[0x14] | 1
 
-        # 6. wait csts.rdy to 1
-        while not (nvme0[0x1c]&0x1) == 1: pass
+    # 6. wait csts.rdy to 1
+    while not (nvme0[0x1c]&0x1) == 1: pass
 
-        # 7. identify controller
-        nvme0.identify(Buffer(4096)).waitdone()
+    # 7. identify controller
+    nvme0.identify(Buffer(4096)).waitdone()
+    
+    # 8. create and identify all namespace
+    nvme0.init_ns()
 
-        # 8. create and identify all namespace
-        nvme0.init_ns()
+    # 9. set/get num of queues
+    nvme0.setfeatures(0x7, cdw11=0x00ff00ff).waitdone()
+    nvme0.getfeatures(0x7).waitdone()
 
-        # 9. set/get num of queues
-        nvme0.setfeatures(0x7, cdw11=0x00ff00ff).waitdone()
-        nvme0.getfeatures(0x7).waitdone()
 
-        # 10. send out all aer
-        aerl = nvme0.id_data(259)+1
-        for i in range(aerl):
-            nvme0.aer()
+def test_ioworker_with_wrr(pcie):
+    nvme0 = Controller(pcie, nvme_init_func=nvme_init_wrr)
+    
+    if (nvme0.cap>>17) & 0x1 == 0:
+        pytest.skip("WRR is not supported")
 
-    nvme0 = Controller(pcie, nvme_init_func=nvme_init)
+    # 8:4:2
+    assert nvme0[0x14] == 0x00460801
+    nvme0.setfeatures(1, cdw11=0x07030103).waitdone()
+    cdw0 = nvme0.getfeatures(1, cdw11=0x7313).waitdone()
+    assert cdw0 == 0x07030103
+
+    nvme0n1 = Namespace(nvme0, 1, 0x10000)
+    nvme0n1.format(512)
+
+    l = []
+    for i in range(3):
+        a = nvme0n1.ioworker(io_size=8,
+                             read_percentage=100,
+                             region_end=0x10000,
+                             qprio=i+1,
+                             time=10)
+        l.append(a)
+
+    w = []
+    for a in l:
+        r = a.start()
+        w.append(r)
+        
+    io_count = []
+    for a in l:
+        r = a.close()
+        logging.debug(r)
+        io_count.append(r.io_count_read)
+
+    logging.info(io_count)
+    assert io_count[1]/io_count[2] > 1.8
+    assert io_count[1]/io_count[2] < 2.2
+    assert io_count[0]/io_count[1] > 1.8
+    assert io_count[0]/io_count[1] < 2.2
+
+    
+def test_weighed_round_robin(pcie):
+    nvme0 = Controller(pcie, nvme_init_func=nvme_init_wrr)
     
     if (nvme0.cap>>17) & 0x1 == 0:
         pytest.skip("WRR is not supported")
 
     assert nvme0[0x14] == 0x00460801
-    nvme0.setfeatures(1, cdw11=0x7313).waitdone()
+    nvme0.setfeatures(1, cdw11=0x07030103).waitdone()
     cdw0 = nvme0.getfeatures(1, cdw11=0x7313).waitdone()
-    assert cdw0 == 0x7313
+    assert cdw0 == 0x07030103
         
     start = time.time()
     nvme0.getfeatures(7).waitdone()
@@ -81,7 +119,7 @@ def test_weighed_round_robin(pcie):
     
     # check sqid of the whole cq
     time.sleep(1)
-    #logging.info([cq[i][2]>>16 for i in range(100*8)])
+    logging.info([cq[i][2]>>16 for i in range(100*8)])
     # assert all urgent IO completed first
     last_sqid = {cq[i][2]>>16 for i in range(300, 800)}
     assert 1 not in last_sqid
