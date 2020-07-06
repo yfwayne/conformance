bd8c3cb7fe2d9ebc770b2ae478e01da01da6fac9
Crane Chu <cranechu@gmail.com>
2020-07-07 07:39:32 +0800

    reset: move to features folder

--- 03_features/reset_test.py
+++ 03_features/reset_test.py
@@ -18,6 +18,7 @@
 # -*- coding: utf-8 -*-
 
 
+import time
 import pytest
 import logging
 
@@ -52,3 +53,58 @@ def test_power_and_reset(pcie, nvme0, subsystem):
     nvme0.reset()              # controller reset: CC.EN
     nvme0.getfeatures(7).waitdone()
 
+
+@pytest.mark.parametrize("delay", [1, 0.1, 0.01, 0.001, 0.0001, 0])
+def test_reset_with_outstanding_io(nvme0, nvme0n1, delay, io_count=1000):
+    nvme0n1.format(512)
+    logging.debug("format done")
+    
+    cq = IOCQ(nvme0, 1, 1024, PRP(1024*64))
+    sq = IOSQ(nvme0, 1, 1024, PRP(1024*64), cqid=1)
+
+    write_cmd = SQE(1, 1)
+    write_cmd[12] = 7  # 4K write
+    buf_list = []
+    for i in range(io_count):
+        buf = PRP(ptype=32, pvalue=i)  # use cid as data pattern
+        buf_list.append(buf)
+        write_cmd.prp1 = buf
+        write_cmd.cid = i
+        write_cmd[10] = i
+        sq[i] = write_cmd
+    sq.tail = io_count
+
+    # reset while io is active
+    logging.debug("write done")
+    time.sleep(delay)
+    nvme0.reset()
+    
+    # read after reset with outstanding writes
+    cq = IOCQ(nvme0, 1, 1024, PRP(1024*64))
+    sq = IOSQ(nvme0, 1, 1024, PRP(1024*64), cqid=1)
+
+    read_cmd = SQE(2, 1)
+    read_cmd[12] = 7  # 4K read
+    buf_list = []
+    for i in range(io_count):
+        buf = PRP()
+        buf_list.append(buf)
+        read_cmd.prp1 = buf
+        read_cmd.cid = i
+        read_cmd[10] = i
+        sq[i] = read_cmd
+    sq.tail = io_count
+    logging.debug("read done")
+
+    # data verify
+    time.sleep(1)
+    assert cq[io_count-1].p != 0
+    for i in range(io_count):
+        cid = cq[i].cid
+        dp = buf_list[cq[i].cid].data(3, 0)  # check data pattern
+        logging.info("cpl %d: cid %d, data pattern %d" % (i, cid, dp))
+        assert dp == 0 or dp == cid 
+    sq.delete()
+    cq.delete()
+    logging.debug("verify done")
+    
