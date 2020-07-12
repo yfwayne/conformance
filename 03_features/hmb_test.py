70812f90993932279e470047957f734e91a5ddc1
Crane Chu <cranechu@gmail.com>
2020-07-12 14:29:04 +0800

    hmb: add more cases

--- 03_features/hmb_test.py
+++ 03_features/hmb_test.py
@@ -26,6 +26,34 @@ from nvme import Controller, Namespace, Buffer, Qpair, Pcie, Subsystem
 from scripts.psd import IOCQ, IOSQ, PRP, PRPList, SQE, CQE
 
 
+@pytest.fixture(scope="function")
+def hmb(nvme0):
+    hmb_size = nvme0.id_data(275, 272)
+    if hmb_size == 0:
+        pytest.skip("hmb is not supported")
+    
+    hmb_buf = d.Buffer(4096*hmb_size)
+    assert hmb_buf
+    hmb_list_buf = d.Buffer(4096)
+    assert hmb_list_buf
+        
+    hmb_list_buf[0:8] = hmb_buf.phys_addr.to_bytes(8, 'little')
+    hmb_list_buf[8:12] = hmb_size.to_bytes(4, 'little')
+    hmb_list_phys = hmb_list_buf.phys_addr
+
+    # enable hmb
+    nvme0.setfeatures(0x0d,
+                      cdw11=1,
+                      cdw12=hmb_size,
+                      cdw13=hmb_list_phys&0xffffffff,
+                      cdw14=hmb_list_phys>>32,
+                      cdw15=1).waitdone()
+    yield
+
+    # disable hmb
+    nvme0.setfeatures(0x0d, cdw11=0).waitdone()
+
+
 def test_single_hmb(nvme0, nvme0n1, hmb):
     hmb_size = nvme0.id_data(275, 272)
     if hmb_size == 0:
@@ -84,4 +112,45 @@ def test_reset_with_hmb_disabled(nvme0, nvme0n1, buf):
     logging.info(hmb_buf.dump(4096*2))
     del hmb_buf
     del hmb_list_buf
+    
+
+def test_multiple_hmb_buffer(nvme0, nvme0n1, buf):
+    hmb_size = nvme0.id_data(275, 272)
+    if hmb_size == 0:
+        pytest.skip("hmb is not supported")
+        
+    # single host memory buffer
+    hmb_buffer_list = []
+    hmb_list_buf = Buffer(hmb_size*16)
+    assert hmb_list_buf
+    for i in range(hmb_size):
+        hmb_buf = Buffer(4096*hmb_size)
+        assert hmb_buf
+        hmb_buffer_list.append(hmb_buf)
+        hmb_list_buf[16*i+0 : 16*i+8] = hmb_buf.phys_addr.to_bytes(8, 'little')
+        hmb_list_buf[16*i+8 : 16*i+12] = hmb_size.to_bytes(4, 'little')
+    hmb_list_phys = hmb_list_buf.phys_addr
+
+    # enable hmb    
+    nvme0.setfeatures(0x0d,
+                      cdw11=1,
+                      cdw12=hmb_size,
+                      cdw13=hmb_list_phys&0xffffffff,
+                      cdw14=hmb_list_phys>>32,
+                      cdw15=1).waitdone()
+    
+    for i in range(3):
+        logging.info(i)
+        with nvme0n1.ioworker(io_size=8,
+                              lba_random=False,
+                              qdepth=8,
+                              read_percentage=0,
+                              time=3):
+            pass
+
+    #disable hmb
+    nvme0.setfeatures(0x0d, cdw11=0).waitdone()
+    del hmb_buffer_list
+    del hmb_list_buf
+    
     
