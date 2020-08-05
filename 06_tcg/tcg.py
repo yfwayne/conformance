194943e609ba5fc07f8463f396589037e551a8dc
Crane Chu <cranechu@gmail.com>
2020-08-05 08:27:46 +0800

    tcg: add tokens

--- 06_tcg/tcg.py
+++ 06_tcg/tcg.py
@@ -42,7 +42,44 @@ import logging
 from nvme import *
 
 
-class OPAL_UID(enum.Enum):
+class OPAL_TOKEN(enum.IntEnum):
+    TRUE = 0x01
+    FALSE = 0x00
+    TABLE = 0x00
+    STARTROW = 0x01
+    ENDROW = 0x02
+    STARTCOLUMN = 0x03
+    ENDCOLUMN = 0x04
+    VALUES = 0x01
+    PIN = 0x03
+    RANGESTART = 0x03
+    RANGELENGTH = 0x04
+    READLOCKENABLED = 0x05
+    WRITELOCKENABLED = 0x06
+    READLOCKED = 0x07
+    WRITELOCKED = 0x08
+    ACTIVEKEY = 0x0A
+    MAXRANGES = 0x04
+    MBRENABLE = 0x01
+    MBRDONE = 0x02
+    HOSTPROPERTIES = 0x00
+    STARTLIST = 0xF0
+    ENDLIST = 0xF1
+    STARTNAME = 0xF2
+    ENDNAME = 0xF3
+    CALL = 0xF8
+    ENDOFDATA = 0xF9
+    ENDOFSESSION = 0xFA
+    STARTTRANSACTON = 0xFB
+    ENDTRANSACTON = 0xFC
+    EMPTYATOM = 0xFF
+    WHERE = 0x00
+    LIFECYCLE = 0x06
+    AUTH_ENABLE = 0x05
+    BOOLEAN_EXPR = 0x03
+
+        
+class OPAL_UID(enum.IntEnum):
     # user
     SMUID = 0
     THISSP = 1 
@@ -76,7 +113,7 @@ class OPAL_UID(enum.Enum):
     HALF_BOOLEAN_ACE = 23
 
 
-class OPAL_METHOD(enum.Enum):
+class OPAL_METHOD(enum.IntEnum):
     PROPERTIES = 0
     STARTSESSION = 1
     REVERT = 2
@@ -141,6 +178,7 @@ class Command(Buffer):
         self.pos = 0x38
 
     def append_u8(self, val):
+        logging.debug(val)
         self[self.pos:] = struct.pack('>B', val)
         self.pos += 1
 
@@ -156,23 +194,48 @@ class Command(Buffer):
         self.append_8_u8(opal_uid_table[u])
         
     def append_token_method(self, m):
+        logging.debug(m)
         self.append_u8(0xa8)
         self.append_8_u8(opal_method_table[m])
+
+    def append_token_list(self, *val_list):
+        for val in val_list:
+            self.append_u8(int(val))
         
     def start_anybody_adminsp_session(self):
         self.append_token_call()
         self.append_token_uid(OPAL_UID.SMUID)
         self.append_token_method(OPAL_METHOD.STARTSESSION)
-        self.append_u8(0xf0)
-        self.append_u8(0x81)
-        self.append_u8(0x69)
-        self.append_token_uid(OPAL_UID.ANYBODY)
-        self.append_u8(1)
-        self.append_u8(0xf1)
-        self[0x39+9+9+3+9+2:] = struct.pack('>BBBBBB', 0xf9, 0xf0, 0, 0, 0, 0xf1)
+        self.append_token_list(0xf0, 0x81, 0x69)
+        self.append_token_uid(OPAL_UID.ADMINSP)
+        self.append_token_list(1, 0xf1)
+        self.append_token_list(0xf9, 0xf0, 0, 0, 0, 0xf1)
         self[16:] = struct.pack('>I', 0x4c)
         self[40:] = struct.pack('>I', 0x34)
         self[52:] = struct.pack('>I', 0x27)
+
+    def get_msid_cpin_pin(self):
+        self.append_token_call()
+        self.append_token_uid(OPAL_UID.C_PIN_MSID)
+        self.append_token_method(OPAL_METHOD.GET)
+        self.append_token_list(OPAL_TOKEN.STARTLIST,
+                               OPAL_TOKEN.STARTLIST,
+                               OPAL_TOKEN.STARTNAME,
+                               OPAL_TOKEN.STARTCOLUMN,
+                               OPAL_TOKEN.PIN,
+                               OPAL_TOKEN.ENDNAME,
+                               OPAL_TOKEN.STARTNAME,
+                               OPAL_TOKEN.ENDCOLUMN,
+                               OPAL_TOKEN.PIN,
+                               OPAL_TOKEN.ENDNAME,
+                               OPAL_TOKEN.ENDLIST,
+                               OPAL_TOKEN.ENDLIST)
+        self.append_token_list(0xf9, 0xf0, 0, 0, 0, 0xf1)
+        self[16:] = struct.pack('>I', 0x4c)
+        self[20:] = struct.pack('>I', 0x101d)
+        self[24:] = struct.pack('>I', 0x69)
+        self[40:] = struct.pack('>I', 0x34)
+        self[52:] = struct.pack('>I', 0x25)
         
 
 class Responce(Buffer):
@@ -204,10 +267,11 @@ def test_pyrite_discovery0(nvme0):
     r.level0_discovery()
 
     
-def test_start_anybody_session(nvme0):
+def test_take_ownership(nvme0):
     r = Responce()
     nvme0.security_receive(r, 1, size=2048).waitdone()
     comid = r.level0_discovery()
+    logging.info(r.dump(256))
     
     c = Command(comid)
     c.start_anybody_adminsp_session()
@@ -216,7 +280,14 @@ def test_start_anybody_session(nvme0):
     nvme0.security_send(c, comid, size=2048).waitdone()
     nvme0.security_receive(r, comid, size=2048).waitdone()
     logging.info(r.dump(256))
+
+    c = Command(comid)
+    c.get_msid_cpin_pin()
+    logging.info(c.dump(256))
     
+    nvme0.security_send(c, comid, size=2048).waitdone()
+    nvme0.security_receive(r, comid, size=2048).waitdone()
+    logging.info(r.dump(256))
     
     
 
