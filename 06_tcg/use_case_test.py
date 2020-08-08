5ea856ab34e53e4235043598fd4db10f8e4b97a4
Crane Chu <cranechu@gmail.com>
2020-08-08 08:17:53 +0800

    tcg: add test activate locking sp

--- 06_tcg/use_case_test.py
+++ 06_tcg/use_case_test.py
@@ -75,3 +75,38 @@ def test_uct03_take_ownership(nvme0, new_passwd):
     tcg.Command(nvme0, comid).revert_tper(hsn, tsn).send()
     tcg.Response(nvme0, comid).receive()
     
+
+def test_uct04_activate_locking_sp(nvme0, new_passwd=b'123456'):
+    # discovery
+    comid = tcg.Response(nvme0).receive().level0_discovery()
+
+    # take ownership
+    tcg.Command(nvme0, comid).start_anybody_adminsp_session(0x65).send()
+    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
+    tcg.Command(nvme0, comid).get_msid_cpin_pin(hsn, tsn).send()
+    password = tcg.Response(nvme0, comid).receive().get_c_pin_msid()
+    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
+    tcg.Response(nvme0, comid).receive()
+
+    tcg.Command(nvme0, comid).start_adminsp_session(0x66, password).send()
+    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
+    tcg.Command(nvme0, comid).set_sid_cpin_pin(hsn, tsn, new_passwd).send()
+    tcg.Response(nvme0, comid).receive()
+    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
+    tcg.Response(nvme0, comid).receive()
+
+    # activate locking sp
+    tcg.Command(nvme0, comid).start_adminsp_session(0x66, new_passwd).send()
+    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
+    tcg.Command(nvme0, comid).get_locking_sp_lifecycle(hsn, tsn).send()
+    tcg.Response(nvme0, comid).receive().get_locking_sp_lifecycle()
+    tcg.Command(nvme0, comid).activate(hsn, tsn).send()
+    tcg.Response(nvme0, comid).receive()
+    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
+    tcg.Response(nvme0, comid).receive()    
+    
+    # revert
+    tcg.Command(nvme0, comid).start_adminsp_session(0x69, new_passwd).send()
+    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
+    tcg.Command(nvme0, comid).revert_tper(hsn, tsn).send()
+    tcg.Response(nvme0, comid).receive()
