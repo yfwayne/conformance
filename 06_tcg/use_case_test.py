9bf9486d92a73da8851d72a23d5e656c60d99118
Crane Chu <cranechu@gmail.com>
2020-08-09 22:22:45 +0800

    tcg: add fixture comid

--- 06_tcg/use_case_test.py
+++ 06_tcg/use_case_test.py
@@ -25,6 +25,11 @@ import nvme as d
 import scripts.tcg as tcg
 
 
+@pytest.fixture()
+def comid(nvme0):
+    return tcg.Response(nvme0).receive().level0_discovery()
+
+
 def test_uct01_level0_discovery(nvme0):
     comid = tcg.Response(nvme0).receive().level0_discovery()
     assert comid >= 1
@@ -50,10 +55,7 @@ def test_uct02_properties(nvme0):
                           b'123456789abcdef',
                           b'123456789abcdef0',
                           b'123456789abcdef0123456789abcdef0'])
-def test_uct03_take_ownership(nvme0, new_passwd):
-    # discovery
-    comid = tcg.Response(nvme0).receive().level0_discovery()
-
+def test_uct03_take_ownership(nvme0, comid, new_passwd):
     # take ownership
     tcg.Command(nvme0, comid).start_anybody_adminsp_session(0x65).send()
     hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
@@ -79,10 +81,7 @@ def test_uct03_take_ownership(nvme0, new_passwd):
     nvme0.timeout = orig_timeout
 
 
-def test_uct04_activate_locking_sp(nvme0, new_passwd=b'123456'):
-    # discovery
-    comid = tcg.Response(nvme0).receive().level0_discovery()
-
+def test_uct04_activate_locking_sp(nvme0, comid, new_passwd=b'123456'):
     # take ownership
     tcg.Command(nvme0, comid).start_anybody_adminsp_session(0x65).send()
     hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
@@ -108,45 +107,8 @@ def test_uct04_activate_locking_sp(nvme0, new_passwd=b'123456'):
     tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
     tcg.Response(nvme0, comid).receive()
 
-    # revert
-    orig_timeout = nvme0.timeout
-    nvme0.timeout = 100000
-    tcg.Command(nvme0, comid).start_adminsp_session(0x69, new_passwd).send()
-    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
-    tcg.Command(nvme0, comid).revert_tper(hsn, tsn).send()
-    tcg.Response(nvme0, comid).receive()
-    nvme0.timeout = orig_timeout
 
-
-def test_uct05_configuring_authorities(nvme0, new_passwd=b'123456'):
-    # discovery
-    comid = tcg.Response(nvme0).receive().level0_discovery()
-
-    # take ownership
-    tcg.Command(nvme0, comid).start_anybody_adminsp_session(0x65).send()
-    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
-    tcg.Command(nvme0, comid).get_msid_cpin_pin(hsn, tsn).send()
-    password = tcg.Response(nvme0, comid).receive().get_c_pin_msid()
-    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
-    tcg.Response(nvme0, comid).receive()
-
-    tcg.Command(nvme0, comid).start_adminsp_session(0x66, password).send()
-    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
-    tcg.Command(nvme0, comid).set_sid_cpin_pin(hsn, tsn, new_passwd).send()
-    tcg.Response(nvme0, comid).receive()
-    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
-    tcg.Response(nvme0, comid).receive()
-
-    # activate locking sp
-    tcg.Command(nvme0, comid).start_adminsp_session(0x66, new_passwd).send()
-    hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
-    tcg.Command(nvme0, comid).get_locking_sp_lifecycle(hsn, tsn).send()
-    tcg.Response(nvme0, comid).receive().get_locking_sp_lifecycle()
-    tcg.Command(nvme0, comid).activate(hsn, tsn).send()
-    tcg.Response(nvme0, comid).receive()
-    tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
-    tcg.Response(nvme0, comid).receive()
-
+def test_uct05_configuring_authorities(nvme0, comid, new_passwd=b'123456'):
     # enable user1 and passwd
     tcg.Command(nvme0, comid).start_adminsp_session(0x66, new_passwd).send()
     hsn, tsn = tcg.Response(nvme0, comid).receive().start_session()
@@ -171,6 +133,8 @@ def test_uct05_configuring_authorities(nvme0, new_passwd=b'123456'):
     tcg.Command(nvme0, comid).end_session(hsn, tsn).send(False)
     tcg.Response(nvme0, comid).receive()
 
+
+def test_uct12_revert_locking_sp(nvme0, comid, new_passwd=b'123456'):
     # revert
     orig_timeout = nvme0.timeout
     nvme0.timeout = 100000
