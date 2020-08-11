1a83b78a78b448c8c9183f905b38f1fdcd4dc843
Crane Chu <cranechu@gmail.com>
2020-08-11 10:31:16 +0800

    revise test code

--- 05_controller/sqe_cqe_test.py
+++ 05_controller/sqe_cqe_test.py
@@ -76,25 +76,28 @@ def test_cq_sqhd(nvme0):
     assert cq[0][0] == 0
     assert cq[0][1] == 0
 
-def test_p_invert_after_cq_passthrough(nvme0):
+    
+def test_p_invert_after_cq_2_pass(nvme0):
     """
-    The value of the Phase Tag is inverted each pass through the Complete Queue.
+    The value of the Phase Tag is inverted each pass filling the Complete Queue.
     """
+    
     # cqid: 1, PC, depth: 2
     cq = IOCQ(nvme0, 1, 2, PRP())
+    
     # create four SQ, both use the same CQ
-    sq3 = IOSQ(nvme0, 3, 16, PRP(), cqid=1)
-    sq5 = IOSQ(nvme0, 5,16, PRP(), cqid=1)
     sq2 = IOSQ(nvme0, 2, 16, PRP(), cqid=1)
-    sq4 = IOSQ(nvme0, 4,16, PRP(), cqid=1)
+    sq3 = IOSQ(nvme0, 3, 16, PRP(), cqid=1)
+    sq4 = IOSQ(nvme0, 4, 16, PRP(), cqid=1)
+    sq5 = IOSQ(nvme0, 5, 16, PRP(), cqid=1)
+    
     # IO command templates: opcode and namespace
     write_cmd = SQE(1, 1)
-    read_cmd = SQE(2, 1)
 
-    for i in range(2):
-        logging.info("before cqe {} ,value {}".format(i,CQE(cq[i%2]).p))
+    logging.info("before cqe {}, value {}".format(0, CQE(cq[0]).p))
+    logging.info("before cqe {}, value {}".format(1, CQE(cq[1]).p))
 
-    # write in sq3, lba1-lba2, 1 page, aligned
+    # write in sq3
     w1 = SQE(*write_cmd)
     write_buf = PRP(ptype=32, pvalue=0xaaaaaaaa)
     w1.prp1 = write_buf
@@ -107,16 +110,9 @@ def test_p_invert_after_cq_passthrough(nvme0):
     # add some delay, so ssd should finish w1 before w2
     time.sleep(0.1)
 
-    # write in sq5, lba5-lba16, 2 page, non aligned
-    w2 = SQE(*write_cmd)
-    buf1 = PRP(ptype=32, pvalue=0xbbbbbbbb)
-    buf1.offset = 2048
-    w2.prp1 = buf1
-    w2.prp2 = PRP(ptype=32, pvalue=0xcccccccc)
-    w2[10] = 5
-    w2[12] = 11 # 0based
-    w2.cid = 0x567
-    sq5[0] = w2
+    # write in sq5
+    w1.cid = 0x567
+    sq5[0] = w1
     sq5.tail = 1
 
     # cqe for w1
@@ -133,38 +129,25 @@ def test_p_invert_after_cq_passthrough(nvme0):
     assert cqe.cid == 0x567
     assert cqe.sqid == 5
     assert cqe.sqhd == 1
+    cq.head = 0
 
-    cq.head = 2
+    logging.info("after cqe {} ,value {}".format(0, CQE(cq[0]).p))
+    logging.info("after cqe {} ,value {}".format(1, CQE(cq[1]).p))
+    
+    logging.info("before cqe {} ,value {}".format(0, CQE(cq[0]).p))
+    logging.info("before cqe {} ,value {}".format(1, CQE(cq[1]).p))
 
-    for i in range(2):
-        logging.info("after cqe {} ,value {}".format(i,CQE(cq[i%2]).p))
-
-    for i in range(2):
-        logging.info("before cqe {} ,value {}".format(i,CQE(cq[i%2]).p))
-
-    # write in sq3, lba1-lba2, 1 page, aligned
-    w3 = SQE(*write_cmd)
-    write_buf = PRP(ptype=32, pvalue=0xdddddddd)
-    w3.prp1 = write_buf
-    w3[10] = 1
-    w3[12] = 1 # 0based
-    w3.cid = 0x147
-    sq2[0] = w3
+    # write in sq3
+    w1.cid = 0x147
+    sq2[0] = w1
     sq2.tail = 1
 
     # add some delay, so ssd should finish w1 before w2
     time.sleep(0.1)
 
-    # write in sq5, lba5-lba16, 2 page, non aligned
-    w4 = SQE(*write_cmd)
-    buf1 = PRP(ptype=32, pvalue=0xeeeeeeee)
-    buf1.offset = 2048
-    w4.prp1 = buf1
-    w4.prp2 = PRP(ptype=32, pvalue=0xffffffff)
-    w4[10] = 5
-    w4[12] = 11 # 0based
-    w4.cid = 0x167
-    sq4[0] = w4
+    # write in sq5
+    w1.cid = 0x167
+    sq4[0] = w1
     sq4.tail = 1
 
     # cqe for w3
@@ -174,14 +157,16 @@ def test_p_invert_after_cq_passthrough(nvme0):
     assert cqe.sqid == 2
     assert cqe.sqhd == 1
     cq.head = 1
+    
     # cqe for w4
     while CQE(cq[1]).p == 1: pass
     cqe = CQE(cq[1])
     assert cqe.cid == 0x167
     assert cqe.sqid == 4
     assert cqe.sqhd == 1
-    cq.head = 2
-    time.sleep(1)
-    for i in range(2):
-        logging.info("after cqe {} ,value {}".format(i,CQE(cq[i%2]).p))
-
+    cq.head = 0
+    
+    logging.info("after cqe {} ,value {}".format(0, CQE(cq[0]).p))
+    logging.info("after cqe {} ,value {}".format(1, CQE(cq[1]).p))
+    assert CQE(cq[0]).p == 0
+    assert CQE(cq[1]).p == 0
