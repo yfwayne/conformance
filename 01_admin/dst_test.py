f6f96f79f2e0ef6039c6ec5003f53229215ecdf3
moon_qi <moon_qi@ymtc.com>
2020-08-31 08:40:07 -0400

    update dst fail case

--- 01_admin/dst_test.py
+++ 01_admin/dst_test.py
@@ -32,7 +32,7 @@ def test_dst(nvme0, nsid, stc):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(stc, nsid).waitdone()
 
     # check dst log page till no dst in progress
@@ -49,7 +49,7 @@ def test_dst_invalid_namespace(nvme0, nsid, stc):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     with pytest.warns(UserWarning, match="ERROR status: 00/0b"):
         nvme0.dst(stc, nsid).waitdone()
 
@@ -60,7 +60,7 @@ def test_dst_in_progress(nvme0, nsid, stc):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(stc, nsid).waitdone()
 
     with pytest.warns(UserWarning, match="ERROR status: 01/1d"):
@@ -77,44 +77,44 @@ def test_dst_in_progress(nvme0, nsid, stc):
         nvme0.getlogpage(0x6, buf, 32).waitdone()
         logging.info("current dst progress percentage: %d%%" % (buf[1] if(buf[1]) else 100))
 
-        
+
 @pytest.mark.parametrize("nsid", [0, 1, 0xffffffff])
 @pytest.mark.parametrize("stc", [1, 2])
 def test_dst_abort(nvme0, nsid, stc):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(stc, nsid).waitdone()
-    
+
     nvme0.dst(0xf, nsid).waitdone()
 
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-        
+
 
 def test_dst_invalid_stc(nvme0, nsid=1):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
-    with pytest.warns(UserWarning, match="ERROR status: 01/0a"):
+
+    with pytest.warns(UserWarning, match="ERROR status: 00/02"):
         nvme0.dst(0, nsid).waitdone()
-    with pytest.warns(UserWarning, match="ERROR status: 01/0a"):
+    with pytest.warns(UserWarning, match="ERROR status: 00/02"):
         nvme0.dst(0xe, nsid).waitdone()
     nvme0.dst(0xf, nsid).waitdone()
 
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
 
-    
+
 @pytest.mark.parametrize("stc", [1, 2])
 def test_dst_abort_by_format(nvme0, nvme0n1, stc, nsid=1):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(stc, nsid).waitdone()
 
     nvme0.format(0, 0).waitdone()
@@ -122,17 +122,17 @@ def test_dst_abort_by_format(nvme0, nvme0n1, stc, nsid=1):
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
 
 def test_dst_short_abort_by_reset(nvme0):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(1, 0).waitdone()
 
     nvme0.reset()
-    
+
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
@@ -142,11 +142,11 @@ def test_dst_extended_abort_by_reset(nvme0):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(2, 0).waitdone()
 
     nvme0.reset()
-    
+
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert buf[0]
@@ -155,23 +155,23 @@ def test_dst_extended_abort_by_reset(nvme0):
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
 
-    
+
 def test_pcie_reset_setup(pcie, nvme0):
     pcie.reset()
     nvme0.reset()
-    
-    
+
+
 def test_dst_extended_abort_by_subsystem_reset(nvme0, subsystem):
     buf = Buffer(4096)
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
-    
+
     nvme0.dst(2, 0).waitdone()
 
     time.sleep(2)
     subsystem.reset()
     nvme0.reset()
-    
+
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert buf[0]
@@ -180,7 +180,7 @@ def test_dst_extended_abort_by_subsystem_reset(nvme0, subsystem):
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
 
-    
+
 @pytest.mark.parametrize("stc", [1, 2])
 def test_dst_abort_by_sanitize(nvme0, nvme0n1, stc, nsid=1):
     if nvme0.id_data(331, 328) == 0:
@@ -194,7 +194,7 @@ def test_dst_abort_by_sanitize(nvme0, nvme0n1, stc, nsid=1):
     def cb(cdw0, status):
         warnings.warn("AER notification is triggered")
     nvme0.aer(cb)
-    
+
     nvme0.dst(stc, nsid).waitdone()
 
     nvme0.sanitize().waitdone()  # sanitize command is completed
@@ -206,13 +206,13 @@ def test_dst_abort_by_sanitize(nvme0, nvme0n1, stc, nsid=1):
             nvme0.getlogpage(0x81, buf, 20).waitdone()
             progress = buf.data(1, 0)*100//0xffff
             logging.info("%d%%" % progress)
-            time.sleep(1)    
+            time.sleep(1)
 
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
 
-    
+
 @pytest.mark.parametrize("stc", [1, 2])
 def test_dst_after_sanitize(nvme0, nvme0n1, stc, nsid=1):
     if nvme0.id_data(331, 328) == 0:
@@ -238,7 +238,7 @@ def test_dst_after_sanitize(nvme0, nvme0n1, stc, nsid=1):
             nvme0.getlogpage(0x81, buf, 20).waitdone()  #L20
             progress = buf.data(1, 0)*100//0xffff
             logging.info("%d%%" % progress)
-        
+
     # check if dst aborted
     nvme0.getlogpage(0x6, buf, 32).waitdone()
     assert not buf[0]
