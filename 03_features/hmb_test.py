b5b3a8407e444db210a690db1debbbc971bca589
Crane Chu <cranechu@gmail.com>
2020-10-07 12:42:23 +0800

    hmb: allocate host buffer at 2MB boundary

--- 03_features/hmb_test.py
+++ 03_features/hmb_test.py
@@ -56,13 +56,46 @@ def hmb(nvme0):
     del hmb_list_buf
 
 
-def test_single_hmb(nvme0, nvme0n1, hmb):
+def test_hmb_single_buffer(nvme0, nvme0n1):
     hmb_size = nvme0.id_data(275, 272)
     if hmb_size == 0:
         pytest.skip("hmb is not supported")
-        
+
+    # allocate host memory
     logging.info(hmb_size)
-    for i in range(3):
+    #hmb_buf = Buffer(hmb_size*4096*2)
+    hmb_buf_2 = Buffer(hmb_size*4096 + 0x200000)  # reserve more buffer
+    #hmb_buf_3 = Buffer(hmb_size*4096*2)
+    #logging.info(hex(hmb_buf.phys_addr))
+    logging.info(hex(hmb_buf_2.phys_addr))
+    #logging.info(hex(hmb_buf_3.phys_addr))
+    #del hmb_buf
+    #del hmb_buf_3
+
+    # build the hmb list by 1MB chunks
+    chunk_size = 0x100000
+    chunk_per_page = chunk_size//4096
+    hmb_list_buf = Buffer(4096)
+    chunk_count = hmb_size*4096//chunk_size
+    logging.info(chunk_count)
+    addr = ((hmb_buf_2.phys_addr+0x200000)>>21)<<21  # align to 2MB
+    for i in range(chunk_count):
+        logging.info(hex(addr))
+        hmb_list_buf[i*16:i*16+8] = addr.to_bytes(8, 'little')
+        hmb_list_buf[i*16+8:i*16+12] = chunk_per_page.to_bytes(4, 'little')
+        addr += chunk_size
+
+    # enable hmb
+    hmb_list_phys = hmb_list_buf.phys_addr
+    nvme0.setfeatures(0x0d,
+                      cdw11=1,
+                      cdw12=hmb_size,
+                      cdw13=hmb_list_phys&0xffffffff,
+                      cdw14=hmb_list_phys>>32,
+                      cdw15=chunk_count).waitdone()
+
+    # test with IO
+    for i in range(10):
         logging.info(i)
         with nvme0n1.ioworker(io_size=8,
                               lba_random=False,
@@ -71,7 +104,14 @@ def test_single_hmb(nvme0, nvme0n1, hmb):
                               time=3):
             pass
 
-        
+    # disable hmb
+    nvme0.setfeatures(0x0d, cdw11=0).waitdone()
+    assert hmb_list_buf
+    assert hmb_buf_2
+    logging.info(hmb_list_buf.dump(64))
+    logging.info(hmb_buf_2.dump(64))
+
+    
 def _test_reset_with_hmb_disabled(nvme0, nvme0n1, buf):
     hmb_size = nvme0.id_data(275, 272)
     if hmb_size == 0:
@@ -121,7 +161,8 @@ def test_multiple_hmb_buffer(nvme0, nvme0n1, buf):
     hmb_size = nvme0.id_data(275, 272)
     if hmb_size == 0:
         pytest.skip("hmb is not supported")
-        
+
+    return
     # single host memory buffer
     hmb_buffer_list = []
     hmb_list_buf = Buffer(hmb_size*16)
